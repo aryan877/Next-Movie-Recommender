2f6c9a8a1466e0af039d7ec5109e9fabed56f9d7
Aryan Kumar <aryankumar877@gmail.com>
2023-02-12 15:05:28 +0530

    modified home page

--- pages/index.js
+++ pages/index.js
@@ -32,14 +32,14 @@ export default function App() {
               <Container styles={{ root: { width: '80%' } }} p={16} mt={210}>
                 <Text align='justify'
                   color='white'
-                  size='lg'>Search your favorite movie and receive a dynamically generated list of similar films. This search is powered by a sophisticated machine learning algorithm written in Python, providing accurate and relevant movie recommendations.
+                  size='lg'>Search over 6000+ English movies and receive a dynamically generated list of similar films. This search is powered by a sophisticated machine learning algorithm written in Python, providing accurate and relevant movie recommendations.
                 </Text>
 
-                <Text align='justify'
+                {/* <Text align='justify'
                   color='white'
                   mt={16}
                   size='md'> Want to discover similar movies? Start by searching for &apos;Annabelle&apos; for a spine-chilling horror experience. Delve into mystery and suspense with &apos;Get Out&apos; or &apos;The Babadook&apos;. Take a trip through the cosmos with &apos;Interstellar&apos;. And for fans of Leo DiCaprio, &apos;Inception&apos; is a must-search for discovering similar mind-bending movies.
-                </Text>
+                </Text> */}
               </Container>
             </MediaQuery>
 
